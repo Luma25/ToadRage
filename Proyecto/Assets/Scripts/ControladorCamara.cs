87c97a8b4dff4ad64395959793aa32e0df79a3ac
Luis Caceres <luimacaroster@gmail.com>
2016-09-05 11:27:32 -0300

    se juega!
    C:

---
+++ Proyecto/Assets/Scripts/ControladorCamara.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorCamara : MonoBehaviour {
+
+	public GameObject Jugador;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - Jugador.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = Jugador.transform.position + offset;
+	}
+}
