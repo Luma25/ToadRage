4f183a78ecbc1c9873e0df068ea22d9e326c7c69
Luis Caceres <luimacaroster@gmail.com>
2016-09-15 12:20:04 -0300

    escenario pulentoso

--- Proyecto/Assets/Scripts/MeleeMovement.cs
+++ Proyecto/Assets/Scripts/MeleeMovement.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MeleeMovement : MonoBehaviour {
+public class MeleeMovement : MonoBehaviour 
+{
+	public Animation anim;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Start () 
+	{
+		anim = GetComponent<Animation>();
+		foreach (AnimationState state in anim) 
+		{
+			state.speed = 0.5F;
+		}
 	}
 }
