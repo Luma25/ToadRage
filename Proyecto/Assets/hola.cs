114018b7e641916d870865a36221ece4ba3a7c6e
eliseo3348 <carloseliseo.fuentes.soto@gmail.com>
2016-09-29 17:37:46 -0300

    holi2

---
+++ Proyecto/Assets/hola.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hola : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.L)) 
+		{	
+			gameObject.GetComponent<Renderer>().material.color=Color.red;
+		}
+
+	}
+}
