a77a8543e098e3af76342695db0d8aae7b8a737e
eliseo3348 <carloseliseo.fuentes.soto@gmail.com>
2016-10-03 15:08:45 -0300

    holi3

--- Proyecto/Assets/hola.cs
+++ Proyecto/Assets/hola.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class hola : MonoBehaviour {
 
-	// Use this for initialization
+//	public gameObject[] cilindro;
+ 	// Use this for initialization
+
 	void Start () {
-	
+	GameObject ParentGameObject = GameObject.FindGameObjectWithTag("Cilindro"); 
+	Transform aChild = transform.FindChild( "Cylinder1" );
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Input.GetKeyDown (KeyCode.L)) 
 		{	
-			gameObject.GetComponent<Renderer>().material.color=Color.red;
+			
 		}
 
 	}
