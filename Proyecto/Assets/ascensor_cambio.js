2f50ab252fce4e3536b0f1a0ee9180c2959306c2
eliseo3348 <carloseliseo.fuentes.soto@gmail.com>
2016-10-17 13:33:27 -0300

    holaaaaaaa

---
+++ Proyecto/Assets/ascensor_cambio.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+function OnCollisionEnter(collision : Collision)
+{
+    if (collision.gameObject.tag == "Player") {
+        Application.LoadLevel ("Ascensor");
+    }
+}
